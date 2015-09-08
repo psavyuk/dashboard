4c54c49c97894ca2682a5636a09e162cd6bfc1ee
psavyuk <psavyuk@gmail.com>
2015-09-08 07:33:54 -0700

    Added drafts files for creation web application.

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return "Hello world"
+
+@app.route('/<mnemonic>', methods=['GET', 'POST'])
+def show_mnemonic(mnemonic):
+    return '%s' % (mnemonic,)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
